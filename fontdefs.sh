5c3d0c182f2a9a92a73337ff644d259e30071cf2
FMAKSIM <fmaksim@gmail.com>
2017-10-30 22:26:11 +0500

    Working

--- fontdefs.sh
+++ fontdefs.sh
@@ -4,5 +4,5 @@ _fonts="/usr/share/fonts/truetype/dejavu"
 for ff in $( find $_fonts -type f ); do
   ff_name=${ff##*/}
   ff_name=${ff_name%.*}
-  fop-ttfreader "$ff" "$HOME/dvlp/btest/$ff_name.xml"
+  fop-ttfreader "$ff" "$HOME/dvlp/dbtest/$ff_name.xml"
 done;
