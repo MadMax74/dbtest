a9d9cbbb68fb8e1fe558055dc5aee0c01a193eb9
FMAKSIM <fmaksim74@gmail.com>
2017-11-02 16:15:47 +0500

    W

--- .fop/font-defs/fontdefs.sh
+++ .fop/font-defs/fontdefs.sh
@@ -4,5 +4,5 @@ _fonts="/usr/share/fonts/truetype/dejavu"
 for ff in $( find $_fonts -type f ); do
   ff_name=${ff##*/}
   ff_name=${ff_name%.*}
-  fop-ttfreader "$ff" "~/.fop/font-defs/$ff_name.xml"
+  fop-ttfreader "$ff" "./$ff_name.xml"
 done;
--- b2pdf.sh
+++ b2pdf.sh
@@ -1,2 +1,2 @@
-xsltproc -o test.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl test.xml
-fop -c fop.xconf -l ru -pdf test.pdf -fo test.fo
+xsltproc --stringparam body.font.family "DejaVuSans" --stringparam title.font.family "DejaVuSerif" --stringparam monospace.font.family "DejaVuSansMono" --output test.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl test.xml
+fop -c ucfg.xml  -l ru -pdf test.pdf -fo test.fo
