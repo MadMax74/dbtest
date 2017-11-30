b1dde91806a85176a649f9662bcacb0533f35275
FMAKSIM <fmaksim74@gmail.com>
2017-11-30 15:51:36 +0500

    It's work!

--- b2pdf_dejavu.sh
+++ b2pdf_dejavu.sh
@@ -1,2 +1,2 @@
-xsltproc --stringparam body.font.family "DejaVuSans" --stringparam title.font.family "DejaVuSerif" --stringparam monospace.font.family "DejaVuSansMono" --output test_dejavu.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl test.xml
+xsltproc --stringparam body.font.family "DejaVuSans" --stringparam title.font.family "DejaVuSerif" --stringparam monospace.font.family "DejaVuSansMono" --stringparam fop1.extensions 1 --output test_dejavu.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl test.xml
 fop -c .fop/font-defs/dejavu/dejavu.xml  -l ru -pdf test_dejavu.pdf -fo test_dejavu.fo
